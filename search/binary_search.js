ced4cbc6121803d01b6500300479087db555646e
androidgrl <kawaharajamie@gmail.com>
2016-10-18 22:42:41 -0500

    binary

---
+++ search/binary_search.js
@@ -0,0 +1,25 @@
+var doSearch = function(array, targetValue) {
+    var min = 0;
+    var max = array.length - 1;
+    var guess;
+    var count = 0;
+    while (min <= max) {
+    count++;
+      guess = Math.floor((max + min) / 2);
+      if (array[guess] === targetValue) {
+          console.log("It took" + count + "times");
+        return guess;
+      } else if (array[guess] < targetValue) {
+        min = guess + 1;
+      } else {
+        max = guess - 1;
+      }
+    }
+    return -1;
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+        41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 73);
+console.log("Found prime at index " + result);
